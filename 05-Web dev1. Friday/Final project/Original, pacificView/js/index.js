dbeedd940619905711fe970e0eb52ee36c6accd1
sarazandi <sara.zandi@gmail.com>
2014-06-19 11:22:25 -0700

    Georg edit in class

--- 05-Web dev1. Friday/Final project/Original, pacificView/js/index.js
+++ 05-Web dev1. Friday/Final project/Original, pacificView/js/index.js
@@ -1,3 +1,32 @@
 $('#toggle-login').click(function(){
   $('#login').toggle();
 });
+
+// bird
+
+/* $('#greybird1').click(function(e){
+    $(this).toggleClass('new-class');
+    e.preventDefault();
+
+
+});
+
+
+*/
+
+// $(document).ready(function () {
+  
+// 	$( "greybird1" ).click(function() {
+// 		alert("Hello World"));
+
+// });
+// }
+
+$(document).ready(function() {
+  
+		alert("Hello World");
+
+});
+
+
+
