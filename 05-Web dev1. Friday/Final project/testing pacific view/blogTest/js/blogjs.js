803199c321c6ac008eec512f80027724c4515286
sarazandi <sara.zandi@gmail.com>
2014-06-19 15:22:43 -0700

    blog animation

---
+++ 05-Web dev1. Friday/Final project/testing pacific view/blogTest/js/blogjs.js
@@ -0,0 +1,26 @@
+	
+// bird
+
+	$(document).ready(function() {
+   alert('hi');
+});
+
+
+
+/* $('#greybird1').click(function(e){
+    $(this).toggleClass('new-class');
+    e.preventDefault();
+
+
+});
+
+
+*/
+
+// $(document).ready(function () {
+  
+// 	$( "greybird1" ).click(function() {
+// 		alert("Hello World"));
+
+// });
+// }
