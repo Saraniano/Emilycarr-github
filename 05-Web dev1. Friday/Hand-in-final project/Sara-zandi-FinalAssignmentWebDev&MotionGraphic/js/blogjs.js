98b66087764d3b094248406884cbe663feb1c219
sarazandi <sara.zandi@gmail.com>
2014-06-20 23:02:22 -0700

    hand in final assignment web dev and motion graphic

---
+++ 05-Web dev1. Friday/Hand-in-final project/Sara-zandi-FinalAssignmentWebDev&MotionGraphic/js/blogjs.js
@@ -0,0 +1,27 @@
+	
+// bird
+
+// 	$(document).ready(function() {
+//    alert('hi');
+// });
+
+$(document).ready(function(){
+	$(".bird").click(function(){
+	    $(this).toggleClass("moved")
+	});
+})
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+  
+// 	$( "greybird1" ).click(function() {
+// 		alert("Hello World"));
+
+// });
+// }
